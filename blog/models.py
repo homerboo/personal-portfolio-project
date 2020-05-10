9c2fb6a6be2bf640b6b994065c41fa3b3fbd0933
homerboo <mikeobw@gmail.com>
2020-05-10 11:24:36 -0400

    Before admin customization

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import date, datetime
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    dateCreated = models.DateField(default=datetime.now)
+    content = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+# from models import Blog
+from .models import Blog 
 
 def all_blogs(request):
-    return render(request,'blogs/blogs.html')
+    # Fetch from db
+    blog = Blog.objects.all()
+    context = {'blogs': blog}
+
+    return render(request,'blogs/blogs.html', context)
